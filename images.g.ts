a24dc1750bddbc116a96703089c497e7e8265bd4
Oygardpc2 <oygardungdomsskole2@gmail.com>
2024-05-06 09:31:56 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,155 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "left":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . f . . . f . . . . . f f f . 
+. . f f . f f . . . . f f f f f 
+. . f f f f f . . . . f f . f f 
+f f 1 f 1 f f f f . . . . . f f 
+. f f f f f f f . . . . . . f f 
+. . f f f f f f f f f f f f f f 
+. . f f f f f f f f f f f f f . 
+. . f f f f f f f f f f f f . . 
+. . f f f f f f f f f f f f . . 
+. . f f . f f . . f f . f f . . 
+. . f f . f f . . f f . f f . . 
+. . f f . f f . . f f . f f . . 
+`;
+            case "image2":
+            case "sit0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f . . . f . . . 
+. . . . . . . . f f . f f . . . 
+. . . . . . . . f f f f f . . . 
+. . . . . . f f f 1 f 1 f f f . 
+. . . . . . . f f f f f f f . . 
+. . . . . . . . f f f f f . . . 
+. . . . . . . . f f f f f . . . 
+. . . . . . . . f f f f f . . . 
+. . . . . . . f f f f f f . . . 
+. . . . . . f f f f f f f . . . 
+. . . . . . f f f f f f f f . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "right":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. f f f . . . . . f . . . f . . 
+f f f f f . . . . f f . f f . . 
+f f . f f . . . . f f f f f . . 
+f f . . . . . f f f f 1 f 1 f f 
+f f . . . . . . f f f f f f f . 
+f f f f f f f f f f f f f f . . 
+. f f f f f f f f f f f f f . . 
+. . f f f f f f f f f f f f . . 
+. . f f f f f f f f f f f f . . 
+. . f f . f f . . f f . f f . . 
+. . f f . f f . . f f . f f . . 
+. . f f . f f . . f f . f f . . 
+`;
+            case "image5":
+            case "up":return img`
+. . . . . f . . . f . . . . . . 
+. . . . . f f . f f . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . f f f f f f f f f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f e f f . . . . . . 
+. . . . . f f e f f . . . . . . 
+. . . . . f f e f f . . . . . . 
+. . . . . f f e f f . . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "down":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . e e f e e . . . . . . 
+. . . . . f e e e f . . . . . . 
+. . . . . f f e f f . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . f f f 1 f 1 f f f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f . f f . . . . . . 
+. . . . . f f . f f . . . . . . 
+. . . . . f f . f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 2 2 2 . . . . . . . . 
+. . . . . 2 . 2 2 2 . . . . . . 
+. . . . 2 2 f . f 2 2 . . . . . 
+. . . . . 2 . . . 2 2 . . . . . 
+. . . . . 2 f . f 2 . . . . . . 
+. . . . . . 2 f 2 . . . . . . . 
+. . . . . 2 2 2 2 . . . . . . . 
+. . 2 2 2 2 . 2 2 2 2 . . . . . 
+. . . . . . . 2 . . 2 2 . . . . 
+. . . . . . . 2 . . . . . . . . 
+. . . . . . 2 2 . . . . . . . . 
+. . . . 2 2 2 2 . . . . . . . . 
+. . 2 2 2 . . 2 2 . . . . . . . 
+. 2 2 . . . . . 2 2 2 . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 2 2 . . . . . . . 
+. . . . . 2 2 . . 2 2 . . . . . 
+. . . . . 2 f . . f 2 . . . . . 
+. . 2 . . 2 . . . . 2 . . 2 . . 
+. . 2 2 . 2 f f f f . . 2 2 . . 
+. . . 2 2 . 2 f f 2 . 2 2 . . . 
+. . . . 2 2 . . 2 . 2 2 . . . . 
+. . . . . . 2 2 2 2 2 . . . . . 
+. . . . . . . . 2 . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . 2 2 2 . . 2 . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,43 @@
+namespace SpriteKind {
+    export const wall = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`up`)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass3, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level9`)
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.darkGroundWest)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`trigger1`, function (sprite, location) {
+	
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
+    if (controller.left.isPressed()) {
+        mySprite.x += 5
+    }
+    if (controller.right.isPressed()) {
+        mySprite.x += -5
+    }
+    if (controller.up.isPressed()) {
+        mySprite.y += 5
+    }
+    if (controller.down.isPressed()) {
+        mySprite.y += -5
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`left`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`right`)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`down`)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`sit0`, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level0`)
+tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
+mySprite.sayText(":)", 500, false)
